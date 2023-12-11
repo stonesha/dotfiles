4b1b180c3d7d4c0f43e6c7488b890e3a42f4ad04
stonesha <stoneshaus@gmail.com>
2023-12-11 15:53:00 -0800

    added barbar and some code runner changes

--- dot_config/nvim/after/plugin/code_runner.lua
+++ dot_config/nvim/after/plugin/code_runner.lua
@@ -1,13 +1,14 @@
-require('code_runner').setup({
-  filetype = {
-    python = "python3 -u",
-    rust = {
-      "cd $dir &&",
-      "rustc $fileName &&",
-      "$dir/$fileNameWithoutExt"
-    },
-  },
+require("code_runner").setup({
+	mode = "tab",
+	filetype = {
+		python = "python3 -u",
+		rust = {
+			"cd $dir &&",
+			"rustc $fileName &&",
+			"$dir/$fileNameWithoutExt",
+		},
+	},
 })
 
-vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>r", ":RunCode<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false })
