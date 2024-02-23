a6282962449943e9212050350a36377a951fb0a5
stonesha <stoneshaus@gmail.com>
2024-02-23 03:12:58 -0800

    changed keymaps and added some tmux support

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- toggle focus on neotree
+vim.keymap.set("n", "<leader>o", function()
+  if vim.bo.filetype == "neo-tree" then
+    vim.cmd.wincmd("p")
+  else
+    vim.cmd.Neotree("focus")
+  end
+end, { desc = "Focus Neotree" })
