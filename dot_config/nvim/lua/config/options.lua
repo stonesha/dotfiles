3d703e8e2d837208efef69ff1ba18e1f1d8b9270
stonesha <stoneshaus@gmail.com>
2024-04-17 21:48:15 -0700

    fixed remaps and sets, also updated plugins

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -15,3 +15,46 @@ vim.api.nvim_create_autocmd("LspAttach", {
     -- whatever other lsp config you want
   end,
 })
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.completeopt = "menuone,noselect"
+
+vim.opt.foldmethod = "indent"
+vim.opt.foldenable = false
+vim.opt.foldlevelstart = 99
+
+-- hacky fix for https://github.com/nvim-telescope/telescope.nvim/issues/2027
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
