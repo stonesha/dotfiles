0adfaeebbef55f2f4d03b2d79591879cf1934d01
Stone Sha <stoneshaus@gmail.com>
2024-06-18 17:17:22 -0700

    add harpoon

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -16,6 +16,13 @@ vim.opt.clipboard = 'unnamedplus'
 -- Enable break indent
 vim.opt.breakindent = true
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
 -- Save undo history
 vim.opt.undofile = true
 
